c8bbb332584d342ba696b6157edaf160c67570aa
Kyle Eriss <keriss@mooveguru.com>
2024-03-08 00:47:58 -0500

    Revising Home and About Me sections

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,40 +1,36 @@
-import { React, Component } from 'react';
-import OttoPicture from '../../images/otto2.JPG';
-import './About.css';
+import { React, Component } from "react";
+import OttoPicture from "../../images/otto2.JPG";
+import "./About.css";
 
 export default class About extends Component {
-    render() {
-        return (
-            <article id='about'>
-                <div className='about'>
+  render() {
+    return (
+      <article id="about">
+        <div className="about">
+          <div className="itemAbout">
+            <div className="title">About Kyle</div>
+              I'm driven by the challenge of solving complex technical problems
+              and creating impactful solutions. With a background in full stack
+              development, data engineering, I thrive in dynamic environments
+              where I can apply my skills to drive innovation.
+            <p>
+              Software development with a customer-centric approach is my focus, and I'm committed to
+              leveraging technology to create a positive experience for users.
+              I'm excited to collaborate with like-minded professionals who
+              share a similar drive to innovate and make a meaningful impact in
+              the digital space.
+            </p>
+            <p>
+              Aside from that, when I'm not coding I'm typically hiking the White Mountains, exploring new places on my
+              motorcycle, or walking on the beach with my dog, Otto.
+            </p>
+          </div>
 
-                    <div className='itemAbout'>
-
-                        <div className='title'>About Kyle</div>
-
-                            I'm focused on removing barriers to employment for underserved communities. I'm ultimately seeking to
-                            combine my experience in workforce development with my tech background to expand access to learning and
-                            tech training opportunities for these communities.
-
-                        <p>
-                            Software development is fun, engaging, and it opens up so many new opportunities. I'd love to work with
-                            a team of like-minded developers who want to inspire others to pursue these opportunities.
-                        </p>
-
-                        <p>
-                            When I'm not coding, I'm typically exploring new places on my motorcycle or walking on the beach with
-                            my dog, Otto.
-                        </p>
-
-                    </div>
-
-                    <div className='itemAbout'>
-                        <img src={OttoPicture} className='profilePictures' />
-                    </div>
-
-                </div>
-            </article>
-
-        )
-    }
+          <div className="itemAbout">
+            <img src={OttoPicture} className="profilePictures" />
+          </div>
+        </div>
+      </article>
+    );
+  }
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,39 +1,37 @@
-import { React, Component } from 'react';
-import './Home.css';
-import YosemiteProfile from '../../images/YosemiteProfile.JPG';
+import { React, Component } from "react";
+import "./Home.css";
+import YosemiteProfile from "../../images/YosemiteProfile.JPG";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <article id='home'>
-                <div className='home'>
+  render() {
+    return (
+      <article id="home">
+        <div className="home">
+          <div className="itemHome">
+            <img src={YosemiteProfile} className="profilePictures" />
+          </div>
 
-                    <div className='itemHome'>
-                        <img src={YosemiteProfile} className='profilePictures' />
-                    </div>
+          <div className="itemHome">
+            <div className="title">Hi. I'm Kyle!</div>
 
-                    <div className='itemHome'>
-
-                    <div className='title'>Hi. I'm Kyle!</div>
-
-                        <div className='itemText'>
-                            I'm a software developer with a background in workforce development living in Boston, MA.
-                        
-
-                            <p>My interests include:</p>
-
-                            <ul className="bulletpoints">
-                                <li>Expanding access to learning and technology</li>
-                                <li>Designing quality websites that engage users</li>
-                                <li>Exploring new places and exchanging ideas with other developers</li>
-                            </ul>
-                        </div>
-
-                    </div>
-
-                </div>
-            </article>
-
-        )
-    }
+            <div className="itemText">
+                 I'm a full stack developer based in Boston, MA, with a passion for
+                 creating innovative digital solutions. My expertise spans front
+                 end design, back end development, and data engineering. My
+                 interests include:
+              <p>My interests include:</p>
+              <ul className="bulletpoints">
+                <li>Crafting user-friendly interfaces and interactive experiences</li>
+                <li>Building scalable and efficient data pipelines</li>
+                <li>
+                  Exploring emerging technologies and frameworks with other
+                  developers
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </article>
+    );
+  }
 }
